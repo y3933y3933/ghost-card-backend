39701529571ab90bf2b08f241acf7a5804df978a
Joanne Lin <y3933y3933@gmail.com>
2025-06-14 19:50:13 +0800

    feat: install gin

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run()
+
 }
